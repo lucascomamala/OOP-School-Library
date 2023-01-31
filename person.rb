51055bf64a3819c4fc9a8a902fe404b22f369dd1
lucascomamala <lucascomamala@gmail.com>
2023-01-31 11:55:57 -0700

    Linter errors

--- person.rb
+++ person.rb
@@ -2,17 +2,19 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name= 'Unknown', parent_permission= true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def is_of_age?
-    @age >= 18
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
-  def can_use_services?
-    is_of_age? || @parent_permission
+  private
+
+  def of_age?
+    @age >= 18
   end
 end
