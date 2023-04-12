b6437a574abe46bc65c8ae265c47ce5dc156ce37
lucascomamala <lucascomamala@gmail.com>
2023-04-12 10:47:03 -0600

    Created trimmer decorator

---
+++ decorators/trimmer_decorator.rb
@@ -0,0 +1,7 @@
+require_relative 'base_decorator'
+
+class TrimmerDecorator < NameableDecorator
+  def correct_name
+     @nameable.length <= 10 ? @nameable.correct_name : @nameable.correct_name[0..9]
+  end
+end
