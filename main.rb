4f04f63284ba37df24ecb2eb93b8ee82818c52a7
lucascomamala <lucascomamala@gmail.com>
2023-04-15 00:07:36 -0600

    Has-many/belongs-to Classroom/Stundent rel

--- main.rb
+++ main.rb
@@ -2,17 +2,19 @@ require_relative './decorators/trimmer_decorator'
 require_relative './decorators/capitalize_decorator'
 require_relative './teacher'
 require_relative './student'
+require_relative './classroom'
 
 t1 = Teacher.new(21, 'Math', name: 'james mcavoy')
 t2 = Teacher.new(25, 'English', name: 'michael fassbender')
-s1 = Student.new(15, '2a', name: 'jennifer lawrence')
-s2 = Student.new(13, '3a', name: 'emma stone', parent_permission: false)
-s3 = Student.new(18, '2b', name: 'josh hutcherson', parent_permission: false)
+p s1 = Student.new(15, name: 'jennifer lawrence')
+s2 = Student.new(13, name: 'emma stone', parent_permission: false)
+s3 = Student.new(18, name: 'josh hutcherson', parent_permission: false)
+class1 = Classroom.new('2a')
+class2 = Classroom.new('3a')
 
-stack = NameableDecorator.new(s1)
-p stack.correct_name
-stack = CapitalizeDecorator.new(stack)
-p stack.correct_name
-stack = TrimmerDecorator.new(stack)
-p stack.correct_name
+p class1.add_student(s1)
+
+p class1.add_student(s2)
+p class1.students
+class2.add_student(s3)
 
